3b1a2d4778713184761e5591daf6aa505704f9ec
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2023-01-11 14:22:09 +0530

    Navbar done

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './navbar.module.css'
+
+function Navbar() {
+  return (
+    // <Link>Navbar</Link>
+    <div className={styles.container}>Navbar</div>
+  )
+}
+
+export default Navbar
